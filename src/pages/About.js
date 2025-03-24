a5e388b27a844a62ea57053a2f2f00d28f7c2cf0
Jason Dhekial Pyrbot <jasonvlog81@gmail.com>
2025-03-24 12:27:41 +0530

    adding navbar section in much more details

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container mt-4">
+      <h1>About WeatherApp</h1>
+      <p>
+        This app provides real-time weather updates using an external API.
+      </p>
+    </div>
+  );
+};
+
+export default About;
