538913ec1a312293fd0330b7046682820ab44fa2
Jason Dhekial Pyrbot <jasonvlog81@gmail.com>
2025-03-24 12:27:16 +0530

    adding navbar section in much more details

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="container mt-4">
+      <h1>Welcome to WeatherApp</h1>
+      <p>Get real-time weather updates for any city in the world.</p>
+    </div>
+  );
+};
+
+export default Home;
